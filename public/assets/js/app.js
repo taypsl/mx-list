8d33accfad78cc6b05b5f33d85b4373d68e63c1e
taypsl <tayparsell@gmail.com>
2017-02-09 17:47:55 -0800

    add and remove song on form with jquery

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	"use-strict";
 
-
-
+// =================================
+// create and cue youtube iframe // not working outside of html file!!
+// =================================
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 
@@ -21,14 +22,32 @@ function onPlayerReady(){
      player.cueVideoByUrl('<%= playlist.songs[i].songURL %>');
 }
 
+// =================================
+// add songs to new playlist form
+// =================================
 
+function addSongToForm() {
+	var newSong = $('.new-song-form');	
+	var addSongButton = $('.add-song-button');
 
+	$(newSong).append(`
+		<div class="song-form">
+			<input type="text" placeholder="artist" name="artist" id="artist">
+			<input type="text" placeholder="song name" name="name" id="name">
+			<input type="text" placeholder="link to image" name="link to image" id="song-imgURL">
+			<input type="text" placeholder="link to song" name="link to song" id="songURL">
+			<input type="text" placeholder="song description" name="song description" id="description">
+			<a href="#" class="remove-song">Remove</a>
+			<hr class="song-line">
+		</div>`);
+}
 
-
-
+function removeSong() {
+	$(this).parent('div').remove();
+}
 
 // =================================
-// event listeners
+// event listeners -> iframe video
 // =================================
 
 $('.title-container').on('click', function(event) {
@@ -38,10 +57,26 @@ $('.title-container').on('click', function(event) {
 
 $('.play').on('click', function(event) {
 	player.playVideo();
+	event.preventDefault();
 })
 
 $('.pause').on('click', function(event) {
 	player.pauseVideo();
+	event.preventDefault();
+})
+
+// =================================
+// event listeners -> add to form
+// =================================
+
+$('.add-song-button').on('click', function(event) {
+	addSongToForm();
+	event.preventDefault();
+})
+
+$('.new-song-form').on('click', '.remove-song', function(event) {
+	$(this).parent('div').remove(); 
+	event.preventDefault();
 })
 
 /*
