c993c7e8d2312ee4a4966c3a424be2487e7c6d97
taypsl <tayparsell@gmail.com>
2017-02-20 22:38:03 -0800

    fixed video play and pause buttons

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,44 +1,5 @@
 $(document).ready(function() {
-	// =================================
-	// create and cue youtube iframe to 
-	// enable custom pause/play buttons
-	// (future feature)
-	// =================================
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-  	var firstScriptTag = document.getElementsByTagName('script')[0];
-  	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-	var player;
-
-	function onYouTubeIframeAPIReady() {
-	    player = new YT.Player('video', {
-	      //videoId: $('.video').attr('href'),  
-	      events: {
-	            onReady: onPlayerReady, 
-	            onState: onStateChange
-	        }
-	    });
-	}
-
-	function onPlayerReady(event) {
-	  // bind events
-	  var pauseButton = document.getElementById("pause");
-	  pauseButton.addEventListener("click", function() {
-	  player.pauseVideo();
-	  });
-	};
-
-	function onStateChange(url) {
-	  player.loadVideoByUrl(url);
-	}
-
-	$(".play-video").on("click", function(event) {
-	  event.preventDefault();
-	  console.log('click');
-	  var thisUrl = $(this).attr("href");
-	  onStateChange(thisUrl);
-	});
 
 	// =================================
 	// add songs to new playlist form
---
+++ public/assets/js/youtube.js
@@ -0,0 +1,42 @@
+// $(document).ready(function() {
+	// =================================
+	// create and cue youtube iframe to 
+	// enable custom pause/play buttons
+	// (future feature)
+	// =================================
+	var tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/iframe_api";
+	  	var firstScriptTag = document.getElementsByTagName('script')[0];
+	  	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		var player;
+
+	function onYouTubeIframeAPIReady() {
+	    player = new YT.Player('video', {
+	      //videoId: $('.video').attr('href'),  
+	      events: {
+	            onReady: onPlayerReady, 
+	            onState: onStateChange
+	        }
+	    });
+	}
+
+	function onPlayerReady(event) {
+	    $(".pause").on("click", function(event) {
+	      event.preventDefault();
+	      player.pauseVideo();
+	    });
+	};
+
+	function onStateChange(url) {
+	  player.loadVideoByUrl(url);
+	}
+
+
+	$(".play-video").on("click", function(event) {
+	  event.preventDefault();
+	  var thisUrl = $(this).attr("href");
+	  onStateChange(thisUrl);
+	});
+
+// });
