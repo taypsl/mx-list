634eef65ca6222f8702eadccff7c9047788fab73
taypsl <tayparsell@gmail.com>
2017-02-21 03:44:45 -0800

    adding wikipedia api call

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -13,37 +13,42 @@ $(document).ready(function() {
 			<div class="new-song-form" id="${songId}">
 
 			<div class="form-group row col-sm-8 col-sm-offset-2">
-			<label class="col-sm-2 col-form-label"></label>
-			<div class="col-sm-10">
-			<input class="form-control artist" type="text" placeholder="artist" name="artist">
-			</div>
-			</div>
-			<div class="form-group row col-sm-8 col-sm-offset-2">
-			<label class="col-sm-2 col-form-label"></label>
-			<div class="col-sm-10">
-			<input class="form-control song-imgURL" type="text" placeholder="link to image" name="song-imgURL">
-			</div>
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<input class="form-control artist" type="text" placeholder="artist" name="artist">
+				</div>
 			</div>
 			<div class="form-group row col-sm-8 col-sm-offset-2">
-			<label class="col-sm-2 col-form-label"></label>
-			<div class="col-sm-10">
-			<input class="form-control name" type="text" placeholder="most essential song" name="name">
-			</div>
-			</div>
-			<div class="form-group row col-sm-8 col-sm-offset-2">
-			<label class="col-sm-2 col-form-label"></label>
-			<div class="col-sm-10">
-			<input class="form-control songURL" type="text" placeholder="link to song" name="songURL">
-			<small id="linkHelp" class="form-text text-muted"> For optimal experience, use a link from <a href="http://www.youtube.com">YouTube.</a></small>
-			</div>
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<input class="form-control song-imgURL" type="text" placeholder="link to image" name="song-imgURL">
+				</div>
 			</div>
 			<div class="form-group row col-sm-8 col-sm-offset-2">
-			<label class="col-sm-2 col-form-label"></label>
-			<div class="col-sm-10">
-			<textarea class="form-control description" type="text" rows="3" cols="50" placeholder="description" name="description"></textarea>
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<input class="form-control name" type="text" placeholder="most essential song" name="name">
+				</div>
 			</div>
-
-			<div class="rs-btn"><a class="remove-song-button" href="#"><span class="glyphicon glyphicon-minus black"></span> Remove</a></div>
+			<div class="form-group row col-sm-8 col-sm-offset-2">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<input class="form-control songURL" type="text" placeholder="link to song" name="songURL">
+				<small id="linkHelp" class="form-text text-muted"> For optimal experience, use a link from <a href="http://www.youtube.com">YouTube.</a></small>
+				</div>
+			</div>
+			<div class="form-group row col-sm-8 col-sm-offset-2">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<input class="form-control wikiExcerpt" type="text" placeholder="link to wikipedia (optional)" name="wikiExcerpt">
+				</div>
+			</div>
+			<div class="form-group row col-sm-8 col-sm-offset-2">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-10">
+					<textarea class="form-control description" type="text" rows="3" cols="50" placeholder="description" name="description"></textarea>
+				</div>
+				<div class="rs-btn"><a class="remove-song-button" href="#"><span class="glyphicon glyphicon-minus black"></span> Remove</a></div>
 			</div>
 
 			<div class="row col-sm-12"><hr></div>
@@ -95,6 +100,7 @@ $(document).ready(function() {
 				name: $(item).find('.name').val(),
 				imgURL: $(item).find('.song-imgURL').val(),
 				songId: getVideoId(songUrlVal),
+				wikiExcerpt: $(item).find('.wikiExcerpt').val(),
 				description: $(item).find('.description').val()
 			}
 			playlistData.songs.push(song)
