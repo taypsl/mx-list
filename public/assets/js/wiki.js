ea8d9d189cb9844170e1900aadefb43894d8024c
taypsl <tayparsell@gmail.com>
2017-02-21 16:56:48 -0800

    need to send multiple pages in one request

--- public/assets/js/wiki.js
+++ public/assets/js/wiki.js
@@ -1,38 +1,62 @@
 
 	// get excerpt from Wikipedia 
 	// find, parse, and return url to query Wikipedia API
+	var countPlaylists = $('.playlistLength').attr('id')
+	console.log(countPlaylists);
+
+
+	var wikiUrls = []
 	function getWikiUrl() {
-		var pageUrl = $('.wikiExcerpt').attr('id');
-		var splitUrl = pageUrl.split('/wiki/');	
-		var titleUrl = splitUrl[1];
-		var percentUrl = titleUrl.replace("_", "%20");
-		console.log(percentUrl);
-		var newUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + percentUrl + '&callback=?';
-		return newUrl;
+		console.log('hello')
+		for(var i=0; i<countPlaylists; i++) {
+			var pageUrl = $(`.wikiExcerpt${i}`).attr('id');
+			console.log(pageUrl)
+			var splitUrl = pageUrl.split('/wiki/');	
+			var titleUrl = splitUrl[1];
+			var percentUrl = titleUrl.replace("_", "%20");
+			// need to request multiple pages with one api call "titles=PageA|PageB|PageC"
+			//then need to disperse page information to each div
+
+
+			console.log(percentUrl);
+			var newUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + percentUrl + '&callback=?';
+			
+			wikiUrls.push(newUrl);
+
+		}
+		// console.log(wikiUrls)
 	}
 
 
+
  	// query Wikipedia API
- 	function getWikiExcerpt() {
- 		$.ajax({
-	        type: 'GET',
-	        url: getWikiUrl(),
-	        contentType: 'application/json; charset=utf-8',
-	        async: false,
-	        dataType: "json",
-	        success: function (data, textStatus, jqXHR) {
-	            var pageId = Object.keys(data.query.pages);
-				var findByPageId = pageId[0];
-				var textract = data.query.pages[findByPageId].extract;
-				var finalExtract = textract.substring(0, 500);
-				$('.wikiExcerpt').html(finalExtract);
-	         },
-	        error: function (errorMessage) {
-	        }
-    	});
+ 	function getWikiExcerpt(url) {
+ 		for(var j=0; j<wikiUrls.length; j++) { 
+	 		$.ajax({
+		        type: 'GET',
+		        url: url,
+		        contentType: 'application/json; charset=utf-8',
+		        async: false,
+		        dataType: "json",
+		        success: function (data, textStatus, jqXHR) {
+		            var pageId = Object.keys(data.query.pages);
+					var findByPageId = pageId[0];
+					var textract = data.query.pages[findByPageId].extract;
+					var finalExtract = textract.substring(0, 500);
+
+					$(`.wikiExcerpt${j}`).html(finalExtract);
+
+		         },
+		        error: function (errorMessage) {
+		        }
+	    	});
+	    }
  	}
+
+
+getWikiExcerpt()
     
-getWikiExcerpt();
+
 
 
 
